3f2948b4557ce827020c8621686810738ebb6641
holdice <hemu0710@qq.com>
2020-06-17 16:37:08 +0800

    BBS后端项目初始化

---
+++ minazuki-bbs-backend/src/main/java/com/minazuki/bbsbackend/BBSBackendApplication.java
@@ -0,0 +1,13 @@
+package com.minazuki.bbsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BBSBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BBSBackendApplication.class, args);
+    }
+
+}
---
+++ minazuki-bbs-backend/src/test/java/com/minazuki/bbsbackend/BbsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minazuki.bbsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
