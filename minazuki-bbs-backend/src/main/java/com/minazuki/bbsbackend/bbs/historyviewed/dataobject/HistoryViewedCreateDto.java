b5d9aca16a6edb5dc69054464e69a446eb57ac2b
tmc <494521535@qq.com>
2020-06-26 22:07:51 +0800

    merge

---
+++ minazuki-bbs-backend/src/main/java/com/minazuki/bbsbackend/bbs/historyviewed/dataobject/HistoryViewedCreateDto.java
@@ -0,0 +1,22 @@
+package com.minazuki.bbsbackend.bbs.historyviewed.dataobject;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+@Data
+@NoArgsConstructor
+@ApiModel("创建历史记录入参数据模型")
+public class HistoryViewedCreateDto {
+
+    @ApiModelProperty(value = "用户ID")
+    @NotNull
+    private Integer ownerId;
+
+    @ApiModelProperty(value = "查看主题ID")
+    @NotNull
+    private Integer viewedThemeId;
+
+}
--- minazuki-bbs-backend/src/main/java/com/minazuki/bbsbackend/bbs/historyviewed/service/HistoryViewedService.java
+++ minazuki-bbs-backend/src/main/java/com/minazuki/bbsbackend/bbs/historyviewed/service/HistoryViewedService.java
@@ -1,5 +1,18 @@
 package com.minazuki.bbsbackend.bbs.historyviewed.service;
 
+import com.minazuki.bbsbackend.bbs.historyviewed.dataobject.HistoryViewedCreateDto;
+import com.minazuki.bbsbackend.bbs.historyviewed.pojo.HistoryViewed;
+
+import java.util.List;
+
 public interface HistoryViewedService {
+
+    HistoryViewed getByIndex(Integer id);
+
+    void addHistory(HistoryViewedCreateDto historyViewedCreateDto);
+
+    void deleteHistory(Integer id);
+
+    List<HistoryViewed> getAllHistoryByUserId(Integer userId);
 
 }
